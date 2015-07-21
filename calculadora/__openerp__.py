274f0c640920b416dd4e17c820b3cb8d6ec44c0b
Javier Campana <jcampana@cyg.ec>
2015-07-21 16:28:32 -0500

    [ADD] Base para módulo

---
+++ calculadora/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014-Today
+#    CyG xxxx
+#    @autor: Javier Campana <jcampana@cyg.ec>
+#
+##############################################################################
+{
+    "name": "Calculadora 2.0",
+    "version": "-21.07.2015",
+    "author": "CyG",
+    "website": "http://www.cyg.ec",
+    "category": "Construction",
+    "sequence": 1,
+    "complexity": "expert",
+    "description": """
+    Modulo para caclular operaciones
+    """,
+    "shortdesc": "Calculadora - CyG",
+    "depends": [],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml": [],
+    "application": True,
+    "auto_install": False,
+}
