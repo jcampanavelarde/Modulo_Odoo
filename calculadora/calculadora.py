e9f6b37807ab8de0d9e55528a717778e377f937e
Javier Campana <jcampana@cyg.ec>
2015-07-21 18:02:30 -0500

    [ADD] Funciones de calculo onchange

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -15,5 +15,12 @@ class calculadora(osv.osv):
         'name': fields.char('Nombre', size=64),
         'a': fields.float('A'),
         'b': fields.float('B'),
-        'total': fields.float('Total'),
+        'total': fields.float('Total Suma'),
+        'total_resta': fields.float('Total Resta'),
+        'total_multi': fields.float('Total Multi'),
+        'total_div': fields.float('Total Divi'),
     }
+
+    def alcambiar_variables(self, cr, uid, ids, a, b, context=None):
+        res = {'value': {'total': a + b}}
+        return res
